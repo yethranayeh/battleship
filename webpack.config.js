396c4adf9b1b9674c3bb38d46dafa62a6a287a07
yethranayeh <aktasalper99@gmail.com>
2022-02-02 23:22:58 +0300

    base files

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+/** @format */
+
+const path = require("path");
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist")
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+
+				use: ["style-loader", "css-loader"]
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: [["@babel/preset-env", { targets: "defaults" }]]
+					}
+				}
+			}
+		]
+	}
+};
